1b216b3b4d79bd2cc38f93a43d7b7446d30fcf88
Lennyblub <54448678+Lennyblub@users.noreply.github.com>
2019-08-24 14:14:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-  $.getJSON("https://api.steampowered.com/IEconItems_440/GetSchemaItems/v0001/?key=27BA15820451EDB0EB73F99175DC5591", 
+  $.getJSON("https://api.steampowered.com/IEconItems_440/GetSchemaItems/v0001/?key=4BF49F44F914FE26C921D5966C362704", 
           function (itemdata){
          console.log(itemdata)}); 
