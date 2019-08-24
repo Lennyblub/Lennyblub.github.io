c9c6230485ad06329b5ab7f5923369f75d65a2d9
Lennyblub <54448678+Lennyblub@users.noreply.github.com>
2019-08-24 10:33:41 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<?php 
+ 
+?>
+<!DOCTYPE html>
+<HTML>
+<head>
+    <link rel="shortcut icon" href="">
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous">  
+    </script>
+
+    <script src='script.js'></script>
+       
+    </head>
+
+<BODY>
+    <div id = "output"></div>
+    <div class= "testcontainer">
+    
+    </div>
+    <script>
+var myArr = ["Orange", "Banana", "Mango", "Kiwi" ];
+console.log(myArr);
+</script>
+    
+</BODY>
+</HTML>
---
+++ script.js
@@ -0,0 +1,3 @@
+  $.getJSON("https://api.steampowered.com/IEconItems_440/GetSchemaItems/v0001/?key=27BA15820451EDB0EB73F99175DC5591", 
+          function (itemdata){
+         console.log(itemdata)}); 
